3847fc10c5f6193bc5cdbd9b9ee613fb452f0afb
shahbaz alam <37538599+Shahbaz72@users.noreply.github.com>
2023-12-10 19:12:17 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,29 @@
-import React ,{useState, useRef} from 'react'
+// App.js
+import React, { useState } from 'react';
 import '../styles/App.css';
-import PortalButton from './PortalButton.js';
-import PortalTextArea from './PortalTextArea.js';
+import PortalButton from './PortalButton';
+import PortalTextArea from './PortalTextArea';
 
-//complete the function 
 const App = () => {
+  const [inputValue, setInputValue] = useState('');
 
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
 
+  const handleButtonClick = () => {
+    // Logic to handle text transfer from input to textarea
+    const textArea = document.getElementById('portal-textarea');
+    textArea.value = inputValue;
+  };
 
   return (
-    
-    <div id="main">
-      <input id="input" value={} onChange={}></input><br/>
-      <PortalButton buttonclick={} /><br/>
-      <PortalTextArea value={}/>
+    <div id="root">
+      <input id="input" value={inputValue} onChange={handleInputChange} />
+      <PortalButton buttonClick={handleButtonClick} />
+      <PortalTextArea />
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
